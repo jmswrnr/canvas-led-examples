d700caeb6d72e5ded9a3fa75fe6f5faaf6473910
James Warner <jmswrnr@users.noreply.github.com>
2020-04-13 10:27:29 +0100

    electron style tweaks

--- electron-threejs/main.js
+++ electron-threejs/main.js
@@ -5,9 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    backgroundColor: '#000000',
+    width: 694,
+    height: 427,
     webPreferences: {
       nodeIntegration: true
     }
